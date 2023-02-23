62c0226797696d8623d66125c07e868001513667
inurhuda00 <inurhuda00@gmail.com>
2023-02-23 10:51:57 +0700

    button conditions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,43 @@
 import {
   ConnectWallet,
   useAddress,
+  useConnect,
   useContract,
   useContractRead,
   Web3Button,
-} from "@thirdweb-dev/react";
-import { utils } from "ethers";
-import type { NextPage } from "next";
-import { useState } from "react";
-import styles from "../styles/Home.module.css";
+} from '@thirdweb-dev/react'
+import { utils } from 'ethers'
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  const contractAddress = "0x1c0133a5c54b5ec1f11e6c80097a7dd3fc938b56";
-  const address = useAddress();
-  const [quantity, setQuantity] = useState(0);
-  const { contract } = useContract(contractAddress);
+  const totalMint: number = 10
+  const MAX_AMOUNT_MINT: number = 100
+  const contractAddress: string = '0x1c0133a5c54b5ec1f11e6c80097a7dd3fc938b56'
+
+  const address = useAddress()
+  const [
+    {
+      data: { connected },
+    },
+  ] = useConnect()
+
+  const [quantity, setQuantity] = useState<number>(0)
+  const { contract } = useContract(contractAddress)
+
   const { data: price, isLoading } = useContractRead(
     contract,
-    "priceForAddress",
+    'priceForAddress',
     address,
     quantity
-  );
-
-  console.log(price?.toString());
+  )
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h3 className={styles.title}>
-          Welcome to{" "}
+          Welcome to{' '}
           <a href="https://thirdweb.com/mumbai/0x1C0133A5c54B5Ec1F11E6c80097A7Dd3fc938B56">
             DetectiveGems Genesis Freemint!!!
           </a>
@@ -51,37 +60,43 @@ const Home: NextPage = () => {
           </label>
         </div>
 
-        <div style={{ marginTop: "20px" }}>
-          <Web3Button
-            contractAddress={contractAddress}
-            action={(contract) => {
-              contract.call("claim", address, quantity);
-            }}
-            // isDisabled={!quantity || parseInt(quantity) < 1 || isLoading}
-          >
-            Mint{" "}
-            {price
-              ? `(${
-                  price?.toString() === "0"
-                    ? "Free"
-                    : `${utils.formatEther(price)} ETH`
-                })`
-              : ""}
-          </Web3Button>
-
-          {/* <Web3Button //IF SOLD OUT
-            contractAddress={contractAddress}
-            action={(contract) => {
-              contract.call("claim", address, quantity);
-            }}
-            isDisabled={!quantity || parseInt(quantity) < 1 || isLoading}
-          >
-            SOLD OUT{" "}
-          </Web3Button> */}
-        </div>
+        {connected && (
+          <div style={{ marginTop: '20px' }}>
+            {totalMint === MAX_AMOUNT_MINT ? (
+              <button disabled>SOLD OUT</button>
+            ) : quantity <= 1 ? (
+              <Web3Button
+                contractAddress={contractAddress}
+                action={(contract) => {
+                  contract.call('claim', address, quantity)
+                }}
+                // isDisabled={!quantity || parseInt(quantity) < 1 || isLoading}
+              >
+                Mint{' '}
+                {price
+                  ? `(${
+                      price?.toString() === '0'
+                        ? 'Free'
+                        : `${utils.formatEther(price)} ETH`
+                    })`
+                  : ''}
+              </Web3Button>
+            ) : (
+              <Web3Button
+                contractAddress={contractAddress}
+                action={(contract) => {
+                  contract.call('claim', address, quantity)
+                }}
+                // isDisabled={!quantity || parseInt(quantity) < 1 || isLoading}
+              >
+                Mint
+              </Web3Button>
+            )}
+          </div>
+        )}
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
